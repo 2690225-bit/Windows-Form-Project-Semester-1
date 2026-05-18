c50b6b1fe15888cb7d6833ad4889a0029b6a821f
2690225-bit <2690225@schoolsnet.act.edu.au>
2026-05-18 20:27:26 +1000

    Add files via upload

---
+++ Forms/BuildOrderForm.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace WindowsFormsApp1.Forms
+{
+    partial class BuildOrderForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // BuildOrderForm
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "BuildOrderForm";
+            this.Load += new System.EventHandler(this.BuildOrderForm_Load_1);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ Forms/BuildOrderForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1.Forms
+{
+    public partial class BuildOrderForm : Form
+    {
+        public BuildOrderForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BuildOrderForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BuildProductPanel(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void BuildOrderForm_Load_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
